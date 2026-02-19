33e91948c6b505689cf7e6b9a3caa2778d4500b4
adnan-bhaldar <adnanbhaldar115@gmail.com>
2026-02-19 20:05:46 +0530

    Refactor navbar functionality and improve HTML structure
    - Updated JavaScript for menu button and close button to improve readability.
    - Enhanced HTML structure by adding consistent spacing and indentation.
    - Organized sections in index.html for better clarity and maintainability.
    - Ensured proper closing of tags and improved overall formatting.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@ let navbar = document.querySelector('.header .navbar');
 let menuBtn = document.querySelector('#menu-btn');
 let closeBtn = document.querySelector('#close-navbar');
 
-menuBtn.onclick = () =>{
+menuBtn.onclick = () => {
    navbar.classList.add('active');
 };
 
-closeBtn.onclick = () =>{
-    navbar.classList.remove('active');
- };
+closeBtn.onclick = () => {
+   navbar.classList.remove('active');
+};
 
-window.onscroll = () =>{
+window.onscroll = () => {
    navbar.classList.remove('active');
 };
